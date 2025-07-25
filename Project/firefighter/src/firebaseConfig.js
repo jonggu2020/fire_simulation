2d075f397cc37aec8e966a6bb93140a8d88924ce
sungchan12~ <chanyouunng1@gmail.com>
2025-07-25 19:13:18 +0900

    7/25

--- Project/firefighter/src/firebaseConfig.js
+++ Project/firefighter/src/firebaseConfig.js
@@ -3,27 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth'; 
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage'; // 1. getStorage를 import합니다.
+import { getStorage } from 'firebase/storage';
 
 const FsConfig = {
-    apiKey: "AIzaSyBCjSKvzR1b_S3UheJDTESTlMr0ff5zsxo",
-    authDomain: "fire-simulation-9ffc1.firebaseapp.com",
-    projectId: "fire-simulation-9ffc1",
-    storageBucket: "gs://fire-simulation-9ffc1.firebasestorage.app",
-    messagingSenderId: "458640372825",
-    appId: "1:458640372825:web:9bb4ce0cfca10bdae06d48",
-    measurementId: "G-7K4QJ15EE8"
+    //FireStore
 };
 
 const RtConfig = {
-    apiKey: "AIzaSyCrVZaqFjeaOVikJZiEHGj__BH0sAsl904",
-    authDomain: "ljg2020315018.firebaseapp.com",
-    databaseURL: "https://ljg2020315018-default-rtdb.firebaseio.com",
-    projectId: "ljg2020315018",
-    storageBucket: "ljg2020315018.firebasestorage.app",
-    messagingSenderId: "872774536997",
-    appId: "1:872774536997:web:70e5b7194afa59988d94cc",
-    measurementId: "G-F07QGHJXDY"
+    //RealTimeDB
 };
 
 const FsApp = initializeApp(FsConfig);
